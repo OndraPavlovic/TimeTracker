efb0cdcbe9eb9f966677c0a1b0f16f2636148e1c
OndraPavlovic <ondra.pavlovic@gmail.com>
2023-09-04 10:46:19 +0200

    Added Entities (User, Project Details)

--- TimeTracker.Shared/Entities/Project.cs
+++ TimeTracker.Shared/Entities/Project.cs
@@ -9,6 +9,8 @@ namespace TimeTracker.Shared.Entities
     public class Project : SoftDeleteableEntity
     {
         public required string Name { get; set; }
-        public List<TimeEntry> timeEntries { get; set; } = new List<TimeEntry>();       
+        public List<TimeEntry> timeEntries { get; set; } = new List<TimeEntry>();
+        public ProjectDetails? ProjectDetails { get; set; }
+        public List<User> Users { get; set; } = new List<User>();
     }
 }
